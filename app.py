f73a1f9f4af5b559756b7ce67f66248dd62c293b
Alby Benny <albybennyolakkattu@gmail.com>
2024-05-21 14:19:29 +0530

    Fiie upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from pytube import YouTube
+from flask_sslify import SSLify
+import os
+
+
+app = Flask(__name__)
+
+sslify = SSLify(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    if request.method == 'POST':
+        youtube_url = request.form.get('url')
+        yt = YouTube(youtube_url)
+        video = yt.streams.get_highest_resolution()
+        video_path = video.download(output_path='./downloads')
+        return send_file(video_path, as_attachment=True)
+    else:
+        return redirect(url_for('index'))
